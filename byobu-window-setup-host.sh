1eaf4cc17abdd2185f353c7d4c721bd4bd7df5a3
Bearsgoroar <interactivepants@gmail.com>
2020-02-10 14:10:06 +1000

    Update byobu-window-setup-host.sh

--- byobu-window-setup-host.sh
+++ byobu-window-setup-host.sh
@@ -2,10 +2,10 @@
 byobu new-session -d -s Byobu
 byobu rename-window "Lighthouse"
 byobu send-keys "sudo /opt/nebula/nebula -config /opt/nebula/config.yml" C-m
-byobu new-window -n Node-1
-byobu new-window -n Node-2
-byobu new-window -n Node-3
-byobu new-window -n Node-4
+byobu new-window -n Node
+byobu new-window -n Node
+byobu new-window -n Node
+byobu new-window -n Node
 byobu new-window -n Ansible
 byobu new-window -n Shell
 byobu -2 attach-session -t Byobu
