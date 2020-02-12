d41039629ce7dd7861a9d552907ae72e6ab57350
Bearsgoroar <interactivepants@gmail.com>
2020-02-12 15:32:04 +1000

    Create install-pwsh.sh

---
+++ install-pwsh.sh
@@ -0,0 +1,18 @@
+# Update package lists
+sudo apt-get update
+
+# Install libunwind8 and libssl1.0
+# Regex is used to ensure that we do not install libssl1.0-dev, as it is a variant that is not required
+sudo apt-get install '^libssl1.0.[0-9]$' libunwind8 -y
+
+# Grab the latest tar.gz
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-linux-arm32.tar.gz
+
+# Make folder to put powershell
+mkdir ~/powershell
+
+# Unpack the tar.gz file
+tar -xvf ./powershell-7.0.0-linux-arm32.tar.gz -C ~/powershell
+
+# Start PowerShell from bash with sudo to create a symbolic link
+sudo ~/powershell/pwsh -c New-Item -ItemType SymbolicLink -Path "/usr/bin/pwsh" -Target "\$PSHOME/pwsh" -Force
