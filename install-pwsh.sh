f8b697d7e02bc7b5b575ee61cec710db4f8f168d
Bearsgoroar <interactivepants@gmail.com>
2020-02-12 15:51:08 +1000

    Update install-pwsh.sh

--- install-pwsh.sh
+++ install-pwsh.sh
@@ -6,13 +6,13 @@ sudo apt-get update
 sudo apt-get install '^libssl1.0.[0-9]$' libunwind8 -y
 
 # Grab the latest tar.gz. This is for ARM CPUs
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.0-rc.2/powershell-7.0.0-rc.2-linux-arm64.tar.gz
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.0-rc.2/powershell-7.0.0-rc.2-linux-arm32.tar.gz
 
 # Make folder to put powershell
 mkdir ~/powershell
 
 # Unpack the tar.gz file
-tar -xvf ./powershell-7.0.0-rc.2-linux-arm64.tar.gz -C ~/powershell
+tar -xvf ./powershell-7.0.0-rc.2-linux-arm32.tar.gz -C ~/powershell
 
 # Start PowerShell from bash with sudo to create a symbolic link
-sudo ~/powershell/pwsh -c New-Item -ItemType SymbolicLink -Path "/usr/bin/pwsh" -Target "\$PSHOME/pwsh" -Force
+sudo ln -s ~/powershell/pwsh /usr/bin/pwsh
